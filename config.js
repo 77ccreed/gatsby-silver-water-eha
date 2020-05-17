35a1bdeb710318dd57c5734097eeb9553010624f
77ccreed <kiur.jullis@gmail.com>
2020-05-17 19:44:02 +0300

    algus

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Spectral', // <title>
-  manifestName: 'Spectral',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: '✩ Luxury Beauty Brands - SilverWater', // <title>
+  manifestName: 'SilverWater',
+  manifestShortName: 'Water', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav Srivastava',
-  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  heading: '✩ Luxury Beauty Brands - SilverWater',
+  subHeading: 'Simple Home PURE Perfume. Sales from October 2020',
   // social
   socialLinks: [
     {
